dd6e8a0abd948dbc4099d05e6bf5d313fab9495b
Evan McGorty <evanjamesmcgorty@gmail.com>
2018-08-15 20:26:28 -0400

    somewhat working

--- run.h
+++ run.h
@@ -2,6 +2,8 @@
 #include<string>
 #include<iostream>
 #include<map>
+#include<unordered_set>
+#include<functional>
 #include"expression-evaluator/include/expression-evaluator/expression.h"
 
 using namespace expr;
@@ -75,6 +77,11 @@ expression build(expression const& output, std::map<std::string,expression>& sub
 }
 
 
+
+
+std::unordered_set<std::string> history_of_expressions{};
+
+
 struct implication
 {
 	implication() = default;
@@ -102,17 +109,83 @@ struct implication
 		}
 		return build(output,substitutions);
 	}
+
+	void put_all_subexpressions(expression const& target, std::function<void(expression&&)> to_call)
+	{
+		auto g = target.get_function();
+		if(g)
+		{
+			for(int i = 0; i!= g->arguments.size(); ++i)
+			{
+				put_all_subexpressions(g->arguments[i], std::function<void(expression&&)>([&](expression&& a)
+				{
+					if(a!=expression::make_empty())
+					{
+						expression newtarget = expression(target);
+						newtarget.get_function()->arguments[i] = std::move(a);
+						to_call(std::move(newtarget));
+					}
+				})
+				);
+			}
+		}
+
+		to_call(put(target));
+	}
+
+};
+
+
+std::vector<implication> all_implications{
+	implication(
+		expression::make("true"),
+		expression::make("not(false)")
+	), implication(
+		expression::make("not(false)"),
+		expression::make("true")
+	), implication(
+		expression::make("not(not(=x))"),
+		expression::make("=x")
+	), implication(
+		expression::make("=x"),
+		expression::make("not(not(=x))")
+	)
 };
 
 
 void run()
 {
-    implication a{expression::make("sum(=a,sum(=b,=a))"),expression::make("sum(sum(=a,=b),=a)")};
-	std::cout << "pattern: " << a.pattern << '\n' << "output: " << a.output << '\n' << std::flush;
-	while(true)
+    std::vector<expression> current_expressions{expression::make("not(true)")};
+    std::vector<expression> next_expressions;
+	while(current_expressions.size() != 0 && history_of_expressions.size() < 100)
 	{
-		expression cur;
-		std::cin >> cur;
-		std::cout << a.put(cur) << '\n' << std::flush;
+		for(auto& impl_it : all_implications)
+		{
+			for(auto& expr_it : current_expressions)
+			{
+				impl_it.put_all_subexpressions(expr_it, std::function<void(expression&&)>([&](expression&& a)
+				{
+					if(a != expression::make_empty())
+					{
+						auto l = history_of_expressions.insert(a.str());
+						if(l.second)
+						{
+							std::cout << *l.first << '\n';
+							next_expressions.push_back(std::move(a));
+						}
+					}
+				})
+				);
+			}
+		}
+		std::swap(current_expressions,next_expressions);
+		next_expressions.resize(0);
 	}
+
+	// for(auto& it : history_of_expressions)
+	// {
+	// 	std::cout << it << '\n';
+	// }
+	std::cout << std::flush;
+	system("pause");
 }
